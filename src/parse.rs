6ae9a6a9aac6e91ac013e2a1f794e243e363afcc
Quentin Dufour <quentin@deuxfleurs.fr>
2023-06-13 13:52:34 +0200

    rework parse example

--- src/parse.rs
+++ src/parse.rs
@@ -1,13 +1,18 @@
 use imf_codec::headers;
 
 fn main() {
-    let header = r#"Date: Fri, 21 Nov 1997 09:55:06 -0600
-Subject: Hello
- World
-From: <quentin@example.com>
+    let header = r#"Date: Fri, 21 Nov 1997 10:01:10 -0600
+From: Mary Smith 
+ <mary@example.net>
 Sender: imf@example.com
+Reply-To: "Mary Smith: Personal Account" <smith@home.example>
+To: John Doe <jdoe@machine.example>
+Subject: Re: Saying Hello
+Message-ID: <3456@example.net>
+In-Reply-To: <1234@local.machine.example>
+References: <1234@local.machine.example>
 
-Hello world
+This is a reply to your hello.
 "#;
 
     println!("{:?}", headers::header_section(header));
