0c37f7eb77574565f99c3ce62ffcb3d3dc5dbf0b
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2026-01-23 12:22:39 +0100

    Define a custom equality FuzzEq for fuzz checking; add a derive macro

---
+++ src/fuzz_eq.rs
@@ -0,0 +1,22 @@
+pub use derive_fuzz_eq::FuzzEq;
+
+pub trait FuzzEq {
+    fn fuzz_eq(&self, other: &Self) -> bool;
+}
+
+impl<T: FuzzEq> FuzzEq for Vec<T> {
+    fn fuzz_eq(&self, other: &Self) -> bool {
+        self.len() == other.len() &&
+        self.iter().zip(other.iter()).all(|(x1, x2)| x1.fuzz_eq(x2))
+    }
+}
+
+impl<T: FuzzEq> FuzzEq for Option<T> {
+    fn fuzz_eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (None, None) => true,
+            (Some(x), Some(y)) => x.fuzz_eq(y),
+            (_, _) => false,
+        }
+    }
+}
