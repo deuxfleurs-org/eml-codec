ad816c563dd5d9f086955a499297a703fba8414b
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2026-03-30 21:06:26 +0200

    Handle more instances of Return-Path encountered in the wild

--- src/imf/trace.rs
+++ src/imf/trace.rs
@@ -47,6 +47,12 @@ pub fn return_path(input: &[u8]) -> IResult<&[u8], ReturnPath<'_>> {
             warn!(input = %bytes_to_trace_string(_i), "bare addr-spec in return-path");
             ReturnPath(Some(a))
         }),
+        map(consumed(mailbox::mailbox), |(_i, m)| {
+            // This is not allowed by the RFC but happens in some real-world emails
+            #[cfg(feature = "tracing-recover")]
+            warn!(input = %bytes_to_trace_string(_i), "mailbox in return-path");
+            ReturnPath(Some(m.addrspec))
+        }),
         empty_path
     ))(input)
 }
@@ -61,4 +67,60 @@ fn empty_path(input: &[u8]) -> IResult<&[u8], ReturnPath<'_>> {
         opt(whitespace::cfws),
     ))(input)?;
     Ok((input, ReturnPath(None)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::imf::mailbox::*;
+    use crate::text::words::Atom;
+    use crate::text::misc_token::Word;
+
+    #[test]
+    fn test_return_path() {
+        assert_eq!(
+            return_path(b" <foo@example.com>"),
+            Ok((
+                &b""[..],
+                ReturnPath(Some(AddrSpec {
+                    local_part: LocalPart(vec![
+                        LocalPartToken::Word(Word::Atom(Atom("foo"[..].into())))
+                    ]),
+                    domain: Domain::Atoms(vec![Atom("example"[..].into()), Atom("com"[..].into())]),
+                }))
+            ))
+        );
+    }
+
+    #[test]
+    fn test_return_path_bare() {
+        assert_eq!(
+            return_path(b" foo@example.com "),
+            Ok((
+                &b""[..],
+                ReturnPath(Some(AddrSpec {
+                    local_part: LocalPart(vec![
+                        LocalPartToken::Word(Word::Atom(Atom("foo"[..].into())))
+                    ]),
+                    domain: Domain::Atoms(vec![Atom("example"[..].into()), Atom("com"[..].into())]),
+                }))
+            ))
+        );
+    }
+
+    #[test]
+    fn test_return_path_mailbox() {
+        assert_eq!(
+            return_path(b"abcdef <foo@example.com> "),
+            Ok((
+                &b""[..],
+                ReturnPath(Some(AddrSpec {
+                    local_part: LocalPart(vec![
+                        LocalPartToken::Word(Word::Atom(Atom("foo"[..].into())))
+                    ]),
+                    domain: Domain::Atoms(vec![Atom("example"[..].into()), Atom("com"[..].into())]),
+                }))
+            ))
+        );
+    }
 }
