d40bf60b53b7dc0cb9a8afc65c7948f9c3d47545
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2026-04-03 16:12:44 +0200

    enron dataset: adjust testing script to work with the new trace tool

--- resources/enron/restore.sh
+++ resources/enron/restore.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wget https://download.deuxfleurs.org/datasets/enron_mail_20150507.tar.gz -O - | tar xzf -
+wget https://download.deuxfleurs.org/datasets/enron_mail_20150507.tar.gz -O - | gunzip > enron_mail_20150507.tar
