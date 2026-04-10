73e9e17de3146d81d86a7a4dff764005db746283
Armaël Guéneau <armael.gueneau@ens-lyon.org>
2026-04-10 21:02:14 +0200

    resources/enron: fix test script

--- resources/enron/test.sh
+++ resources/enron/test.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$( dirname -- "$( readlink -f -- "$0"; )"; )"
-cd "$SCRIPT_DIR/../.."
-cargo run --features=tracing-discard --release --example trace -- "$SCRIPT_DIR/enron_mail_20150507.tar" > "$SCRIPT_DIR/trace.json"
+cargo run --features=tracing-unsupported --release --example trace -- ./enron_mail_20150507.tar > ./trace.json
